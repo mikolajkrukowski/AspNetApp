eff1ddfaa34d974c6a98bc4072e253f08328dbcb
Mikołaj Krukowski <mikolajk12@interia.pl>
2024-08-30 16:59:52 +0200

    Layout and index updates

--- aspnetcoreapp/Pages/Movies/Index.cshtml.cs
+++ aspnetcoreapp/Pages/Movies/Index.cshtml.cs
@@ -23,7 +23,10 @@ namespace aspnetcoreapp.Pages_Movies
 
         public async Task OnGetAsync()
         {
-            Movie = await _context.Movie.ToListAsync();
+            if (_context.Movie != null) 
+            {
+                Movie = await _context.Movie.ToListAsync();
+            }
         }
     }
 }
