b6dcf92d6e71d000fe24ea17dec0a728f7c07149
Mikołaj Krukowski <mikolajk12@interia.pl>
2024-09-06 21:08:28 +0200

    Added page filters (idk whats going on here)

---
+++ aspnetcoreapp/Filters/SampleAsyncPageFilter.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using PageFilter;
+
+namespace aspnetcoreapp.Filters
+{
+    public class SampleAsyncPageFilter : IAsyncPageFilter
+    {
+        private readonly IConfiguration _config;
+
+        public SampleAsyncPageFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public Task OnPageHandlerSelectionAsync(PageHandlerSelectedContext context)
+        {
+            var key = _config["UserAgentID"];
+            context.HttpContext.Request.Headers.TryGetValue("user-agent",
+                                                            out StringValues value);
+            ProcessUserAgent.Write(context.ActionDescriptor.DisplayName,
+                                   "SampleAsyncPageFilter.OnPageHandlerSelectionAsync",
+                                   value, key.ToString());
+
+            return Task.CompletedTask;
+        }
+
+        public async Task OnPageHandlerExecutionAsync(PageHandlerExecutingContext context,
+                                                      PageHandlerExecutionDelegate next)
+        {
+            // Do post work.
+            await next.Invoke();
+        }
+    }
+}
---
+++ aspnetcoreapp/Filters/SamplePageFilter.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Primitives;
+using PageFilter;
+using System.Diagnostics;
+
+namespace aspnetcoreapp.Filters
+{
+    public class SamplePageFilter : IPageFilter
+    {
+        private readonly IConfiguration _config;
+
+        public SamplePageFilter(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public void OnPageHandlerSelected(PageHandlerSelectedContext context)
+        {
+            var key = _config["UserAgentID"];
+            context.HttpContext.Request.Headers.TryGetValue("user-agent", out StringValues value);
+            ProcessUserAgent.Write(context.ActionDescriptor.DisplayName,
+                                   "SamplePageFilter.OnPageHandlerSelected",
+                                   value, key.ToString());
+        }
+
+        public void OnPageHandlerExecuting(PageHandlerExecutingContext context)
+        {
+            Debug.WriteLine("Global sync OnPageHandlerExecuting called.");
+        }
+
+        public void OnPageHandlerExecuted(PageHandlerExecutedContext context)
+        {
+            Debug.WriteLine("Global sync OnPageHandlerExecuted called.");
+        }
+    }
+}
